e1d7e04e994102f54116c893f8c60bc1cbb7d016
lesterjefframos <110889434+gammainteractiveinc@users.noreply.github.com>
2022-08-13 12:48:51 +0800

    Update smtp.js

--- js/smtp.js
+++ js/smtp.js
@@ -1,14 +1,14 @@
 function sendEmail (){
     Email.send({
         SecureToken : "08a4f10b-910b-4feb-b04a-520e0bcdfa20",
-        To : 'lester.ramos@live.com',
+        To : 'recruitment@giiph.com',
         From : document.getElementById("email").value,
-        Subject : "This is the subject",
+        Subject : "New Inquiry",
         Body : "Name: " + document.getElementById("name").value
         + "<br> Email: " + document.getElementById("email").value
         + "<br> Contact No.: " + document.getElementById("contact").value
         + "<br> Message: " + document.getElementById("message").value
 }).then(
-  message => alert(message)
+  message => alert("Message sent successfully!")
     );
 }
